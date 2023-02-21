a667dfe1f1b78ad076537788187945a352cd45be
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-20 21:20:47 -0300

    adding theme with colors

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
+import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView } from 'react-native'
 import { Home } from './src/Home'
+import { theme } from './src/styles/theme'
 
 export default function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{ backgroundColor: theme.colors.gray['700'], flex: 1 }}
+    >
+      <StatusBar style="auto" translucent />
       <Home />
     </SafeAreaView>
   )
---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    gray: {
+      100: '#f2f2f2',
+      200: '#d9d9d9',
+      300: '#f7f7f7',
+      400: '#333333',
+      500: '#262626',
+      600: '#1a1a1a',
+      700: '#0d0d0d',
+    },
+    purple: '#8284fa',
+    purpleDark: '#5e60ce',
+    blue: '#4ea8de',
+    blueDark: '#1e6f9f',
+    danger: '#e25858',
+  },
+} as const
